38c8da6899d8991891feaa860dfa06ec7ebfd778
Bhaktik24 <112937668+Bhaktik24@users.noreply.github.com>
2022-09-12 13:41:33 +0530

    Add files via upload

---
+++ MainEmployee.java
@@ -0,0 +1,23 @@
+class MainEmployee
+{
+public static void main(String[] args)
+{
+Employee e1=new Employee();
+Employee e2=new Employee();
+Employee e3=new Employee();
+
+e1.id=1;
+e1.name="bhakti";
+
+e2.id=2;
+e2.name="gaurav";
+
+e3.id=1;
+e3.name="pranay";
+
+e1.displaydata();
+e2.displaydata();
+e3.displaydata();
+
+System.out.println();
+}}
