e76bbdb35ac8a774de33d9ddfcc4be26c2b15f89
Bhaktik24 <112937668+Bhaktik24@users.noreply.github.com>
2022-09-29 13:04:43 +0530

    Add files via upload

---
+++ Lab(29-09-22)/src/Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Palindrome {
+
+	public static void main(String args[])
+	   {
+	      String a, rev = "";
+	      Scanner sc = new Scanner(System.in);
+	 
+	      System.out.println("Enter a string:");
+	      a = sc.nextLine();
+	 
+	      int length = a.length();
+	 
+	      for ( int i = length - 1; i >= 0; i-- )
+	         rev = rev + a.charAt(i);
+	 
+	      if (a.equals(rev))
+	         System.out.println(a+" is a palindrome");
+	      else
+	         System.out.println(a+" is not a palindrome");
+	 
+	   }
+}
