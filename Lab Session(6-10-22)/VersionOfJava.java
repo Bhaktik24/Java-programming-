3272264aeef7cfa8f11efb661f0c8b0f8c7aa1cf
Bhaktik24 <112937668+Bhaktik24@users.noreply.github.com>
2022-10-06 12:21:41 +0530

    Add files via upload

---
+++ Lab Session(6-10-22)/VersionOfJava.java
@@ -0,0 +1,10 @@
+package labsession6;
+
+public class VersionOfJava 
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println("version of java is:-  " + System.getProperty("java.version"));
+	}
+}
