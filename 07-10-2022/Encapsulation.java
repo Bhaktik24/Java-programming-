d23891f1ce253612b72a8c522efc2ef7c4035604
Bhaktik24 <112937668+Bhaktik24@users.noreply.github.com>
2022-10-07 11:06:41 +0530

    Add files via upload

---
+++ 07-10-2022/Encapsulation.java
@@ -0,0 +1,34 @@
+package corejava;
+/*
+ Encapsulation Program
+ name:- Bhakti khandekar
+ date:-7/10/22
+ */
+
+
+public class Encapsulation 
+{
+ private int roll_no;
+ private String name;
+ private String location;
+public int getRoll_no() {
+	return roll_no;
+}
+public void setRoll_no(int roll_no) {
+	this.roll_no = roll_no;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getLocation() {
+	return location;
+}
+public void setLocation(String location) {
+	this.location = location;
+}
+ 
+ 
+}
