5e38b8fc1605f3a1ef43845268c74f1f0f5c860c
Bhaktik24 <112937668+Bhaktik24@users.noreply.github.com>
2022-10-10 13:21:34 +0530

    Add files via upload

---
+++ practicals/Basic/UnaryOpeator.java
@@ -0,0 +1,32 @@
+package corejava.basic;
+
+import java.util.Scanner;
+
+class UnaryOpeator {
+
+	public static void main(String[] args) {
+
+		//Creating Scanner class object to take the input from the user
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter number: ");
+		int num1=sc.nextInt();
+		
+		//performing pre Increment operator
+		++num1;
+		System.out.println("Pre Increment value: "+num1);
+		
+		//performing post Increment operator
+		num1++;
+		System.out.println("Post Increment value: "+num1);
+		
+		//performing pre Decrement operator
+		--num1;
+		System.out.println("Pre Decrement value: "+num1);
+		
+		//performing post Decrement operator
+		num1--;
+		System.out.println("Post Decrement value: "+num1);
+
+	}
+
+}
