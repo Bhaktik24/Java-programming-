3dc89f9afef5396fdbbf7c4e1b17964b71e254f5
Bhaktik24 <112937668+Bhaktik24@users.noreply.github.com>
2022-11-01 19:22:31 +0530

    Add files via upload

---
+++ practicals/Basic/casting/Footballer.java
@@ -0,0 +1,13 @@
+package casting;
+
+public class Footballer extends Player
+{
+	void goal()
+	{
+		System.out.println("This is goal");                                                                                                                                                                                       
+	}
+	void defence()
+	{
+		System.out.println("This is defence");                                                                                                                                                                                       
+	}
+}
